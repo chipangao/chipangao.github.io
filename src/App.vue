5937da9f5aa4dfaa7be0229805f202ce02d1515c
David <grandv12112@gmail.com>
2023-05-12 14:51:21 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -20,7 +20,6 @@ function mounted() {
     <!-- <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" /> -->
     <div class="wrapper">
       <nav>
-        {{config.theme}}
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
--- src/components/Home/Home_Compoents.vue
+++ src/components/Home/Home_Compoents.vue
@@ -17,7 +17,6 @@ export default {
 
 <template>
   <Renderer ref="renderer" >
-    <input ref="input"/>
     <Camera :position="{ z: 10 }" />
     <Scene>
       <PointLight :position="{ y: 50, z: 50 }" />
